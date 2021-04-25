a2e28be7e482766468c1ee367a90e0bb1cb86901
Vadiraj Hippargi <Vadi@leadconsultant.com>
2021-04-25 08:36:29 -0700

    Initial Commit

---
+++ UnderstandingToolbarInSwiftui/UnderstandingToolbarInSwiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnderstandingToolbarInSwiftuiApp.swift
+//  UnderstandingToolbarInSwiftui
+//
+//  Created by Vadiraj Hippargi on 4/25/21.
+//
+
+import SwiftUI
+
+@main
+struct UnderstandingToolbarInSwiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
