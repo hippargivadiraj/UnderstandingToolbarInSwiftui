70baf5b1e33f7da5b2ac835b255d7329a9e7fb41
Vadiraj Hippargi <Vadi@leadconsultant.com>
2021-04-25 08:48:57 -0700

    Another Way of doing Toolbar

---
+++ UnderstandingToolbarInSwiftui/UsingToolbarItem.swift
@@ -0,0 +1,37 @@
+//
+//  UsingToolbarItem.swift
+//  UnderstandingToolbarInSwiftui
+//
+//  Created by Vadiraj Hippargi on 4/25/21.
+//
+
+import SwiftUI
+
+struct UsingToolbarItem: View {
+    
+    let messages: [String] = ["a", "b"]
+    var body: some View {
+        
+        NavigationView{
+        List(messages, id: \.self) { message in
+                   Text(message)
+               }
+               .navigationTitle("Messages")
+               .toolbar {
+                   ToolbarItem(placement: .primaryAction) {
+                       Button("New") {}
+                   }
+
+                   ToolbarItem(placement: .bottomBar) {
+                       Button("Filter") {}
+                   }
+    }
+    }
+    }
+}
+
+struct UsingToolbarItem_Previews: PreviewProvider {
+    static var previews: some View {
+        UsingToolbarItem()
+    }
+}
